cb8b8476de2729a4ed00540742a74bcd977dc8f6
github username <github email address>
2018-08-29 16:27:25 +0100

    Web3.js and Plaid Authentication integrated in app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import PlaidAuthenticator from 'react-native-plaid-link';
 
 import './global';
 
@@ -9,25 +10,105 @@ const web3 = new Web3(
   new Web3.providers.HttpProvider('https://mainnet.infura.io/'),
 );
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    data: {},
+    status: 'LOGIN_BUTTON'
+  };
+
   render() {
-    // printing on the console the latest ethereum block
+    console.log(this.state.status)
     web3.eth.getBlock('latest').then(console.log);
 
+    switch(this.state.status) {
+      case 'CONNECTED':
+        console.log('connected')
+        return this.renderDetails()
+      case 'LOGIN_BUTTON':
+      case 'EXIT':
+        return this.renderButton();
+      default:
+        return this.renderLogin();
+    }
+  }
+
+  renderButton = () => {
+    return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => this.setState({status: ''})}>
+        <Text style={styles.paragraph}>Login with Plaid</Text>
+      </TouchableOpacity>
+    </View>
+    )
+  }
+
+  onLoadStart = props => {
+    console.log('onLoadStart', props);
+  };
+
+  onLoad = props => {
+    console.log('onLoad', props);
+  };
+
+  onLoadEnd = props => {
+    console.log('onLoadEnd', props);
+  };
+
+  renderLogin() {
+    return (
+      <PlaidAuthenticator
+        onMessage={this.onMessage}
+        publicKey="eecc6d6382543dbee6478afbc5879b"
+        env="sandbox"
+        product="auth,transactions"
+        onLoad={this.onLoad}
+        onLoadStart={this.onLoadStart}
+        onLoadEnd={this.onLoadEnd}
+      />
+    );
+  }
+
+  renderDetails() {
     return (
       <View style={styles.container}>
-        <Text>Check your console!</Text>
-        <Text>You should find info on the latest ethereum block.</Text>
+        <Text style={styles.paragraph}>Institution</Text>
+        <Text style={styles.value}>
+          {this.state.data.metadata.institution.name}
+        </Text>
+        <Text style={styles.paragraph}>Institution ID</Text>
+        <Text style={styles.value}>
+          {this.state.data.metadata.institution.institution_id}
+        </Text>
+        <Text style={styles.paragraph}>Token</Text>
+        <Text style={styles.value}>
+          {this.state.data.metadata.public_token}
+        </Text>
       </View>
     );
   }
+
+  onMessage = data => {
+    this.setState({ data, status: data.action.substr(data.action.lastIndexOf(':') + 1).toUpperCase() });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 24,
+    backgroundColor: '#ecf0f1'
   },
+  paragraph: {
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e'
+  },
+  value: {
+    marginBottom: 20,
+    textAlign: 'center'
+  }
 });
